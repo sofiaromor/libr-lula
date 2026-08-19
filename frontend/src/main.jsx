523e60b2a726c82d24ee0a69e598f8a4358d8bf0
Sofía Romor <sofiarmorandeira@gmail.com>
2026-08-19 16:46:44 +0200

    Activar precarga de Inicio

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import MobileReaderDock from "./MobileReaderDock.jsx";
 import GuestAccessGuard from "./GuestAccessGuard.jsx";
+import HomeWarmup from "./HomeWarmup.jsx";
 import "./MobileNavRefinement.css";
 import "./GuestNavFix.css";
 import "./MobilePolish.css";
@@ -12,6 +13,7 @@ import "./CozyDockPreview.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <HomeWarmup />
     <App />
     <MobileReaderDock />
     <GuestAccessGuard />
