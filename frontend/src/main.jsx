70af588b0e445aebc93c4bc0d4d3f4f4c27bf39f
Sofía Romor <sofiarmorandeira@gmail.com>
2026-08-19 14:13:55 +0200

    Aplicar refinamiento de navegación móvil

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./MobileNavRefinement.css";
 import App from "./App.jsx";
 import MobileReaderDock from "./MobileReaderDock.jsx";
 
