b03ffce7b800aa3a3bd7bd58ab3fdae4db82f6b0
Sofía Romor <sofiarmorandeira@gmail.com>
2026-08-19 14:25:16 +0200

    Aplicar guard de invitado y correcciones de cabecera

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./MobileNavRefinement.css";
 import App from "./App.jsx";
 import MobileReaderDock from "./MobileReaderDock.jsx";
+import GuestAccessGuard from "./GuestAccessGuard.jsx";
+import "./MobileNavRefinement.css";
+import "./GuestNavFix.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <MobileReaderDock />
+    <GuestAccessGuard />
   </StrictMode>,
 );
